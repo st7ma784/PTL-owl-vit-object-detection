08a1c792f740424d7b9cb18668eecaa1cc655c5d
steve <s.mander3@lancaster.ac.uk>
2023-07-31 15:08:00 +0100

    seems s oneat with BS 1

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 import json
 import os
 import shutil
-
 import torch
 import yaml
 from torchmetrics.detection.mean_ap import MeanAveragePrecision
 from torchvision.io import write_png
 from tqdm import tqdm
 
+''' This file defines loading OWLVIT with PTL and training in that ecosystem.'''
+
+import torch
+import pytorch_lightning as pl
+from PIL import Image
+from transformers import AutoProcessor, OwlViTForObjectDetection
 from src.losses import PushPullLoss
 from src.dataset import get_dataloaders
-from src.models import PostProcess, load_model
+from src.models import PostProcess, load_model,OwlViT
 from src.train_util import (
     coco_to_model_input,
     labels_to_classnames,
@@ -40,142 +45,154 @@ def init_weights(m):
         print(dir(m))
 
 
-if __name__ == "__main__":
-    device = "cuda" if torch.cuda.is_available() else "cpu"
-    metric = MeanAveragePrecision(iou_type="bbox", class_metrics=True).to(device)
-    epoch_train_losses = GeneralLossAccumulator()
-    train_loss_accumulator, val_loss_accumulator = [GeneralLossAccumulator()] * 2
-    progress_summary = ProgressFormatter()
+class OwlVITModule(pl.LightningModule):
+    def __init__(self, training_cfg,scales=None,labelmap=None):
+        super().__init__()
+        _model = OwlViTForObjectDetection.from_pretrained("google/owlvit-base-patch32")
+        _processor = AutoProcessor.from_pretrained("google/owlvit-base-patch32")
+        self.labelmap=labelmap
+        self.scales=scales
+        inputs = _processor(
+            text=[list(labelmap.values())],
+            images=Image.new("RGB", (224, 224)),
+            return_tensors="pt",
+        )
 
-    if os.path.exists("debug"):
-        shutil.rmtree("debug")
-
-    training_cfg = get_training_config()
-    train_dataloader, test_dataloader, scales, labelmap = get_dataloaders()
-
-    model, trainable = load_model(labelmap, device)
-
-    postprocess = PostProcess(
-        confidence_threshold=training_cfg["confidence_threshold"],
-        iou_threshold=training_cfg["iou_threshold"],
-    )
-
-    criterion = PushPullLoss(
-        len(labelmap),
-        scales=torch.tensor(scales).to(device)
-        if training_cfg["use_class_weight"]
-        else None,
-    )
-
-    optimizer = torch.optim.AdamW(
-        model.parameters(),
-        lr=float(training_cfg["learning_rate"]),
-        weight_decay=training_cfg["weight_decay"],
-        amsgrad=True,
-    )
-    scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.9)
-
-    classMAPs = {v: [] for v in list(labelmap.values())}
-    model.train()
-    for epoch in range(training_cfg["n_epochs"]):
-        print(scheduler.get_lr())
-        # scheduler.step()
-        # continue
-        if training_cfg["save_eval_images"]:
-            os.makedirs(f"debug/{epoch}", exist_ok=True)
-
-        # Train loop
-        for i, (image, labels, boxes, metadata) in enumerate(
-            tqdm(train_dataloader, ncols=60)
-            # train_dataloader
-        ):
-            optimizer.zero_grad()
-
-            # Prep inputs
-            image = image.to(device)
-            labels = labels.to(device)
-            boxes = coco_to_model_input(boxes, metadata).to(device)
-
-            # Predict
-            all_pred_boxes, _, pred_sims, _ = model(image)
-            losses = criterion(pred_sims, labels, all_pred_boxes, boxes)
-            loss = (
-                losses["loss_ce"]
-                + losses["loss_bg"]
-                + losses["loss_bbox"]
-                + losses["loss_giou"]
-            )
-            loss.backward()
-            # print(loss.sum().item())
-            torch.nn.utils.clip_grad_norm_(model.parameters(), 1)
-            optimizer.step()
-
-            train_loss_accumulator.update(losses)
-
-        epoch_train_losses = train_loss_accumulator.get_values()
-        scheduler.step()
-        # Eval loop
-        model.eval()
         with torch.no_grad():
-            for i, (image, labels, boxes, metadata) in enumerate(
-                tqdm(test_dataloader, ncols=60)
-            ):
-                # Prep inputs
-                image = image.to(device)
-                labels = labels.to(device)
-                boxes = coco_to_model_input(boxes, metadata).to(device)
+            queries = _model(**inputs).text_embeds
 
-                # Get predictions and save output
-                pred_boxes, _, pred_class_sims, _ = model(image)
-                losses = criterion(pred_class_sims, labels, pred_boxes, boxes)
-                val_loss_accumulator.update(losses)
+        patched_model = OwlViT(pretrained_model=_model, query_bank=queries)
 
-                pred_boxes, pred_classes, scores = postprocess(
-                    pred_boxes, pred_class_sims
-                )
 
-                # Use only the top 200 boxes to stay consistent with benchmarking
-                top = torch.topk(scores, min(200, scores.size(-1)))
-                scores = top.values
-                inds = top.indices.squeeze(0)
 
-                update_metrics(
-                    metric,
-                    metadata,
-                    pred_boxes[:, inds],
-                    pred_classes[:, inds],
-                    scores,
-                    boxes,
-                    labels,
-                )
+        self.criterion = PushPullLoss(
+                                        len(labelmap),
+                                        scales=torch.tensor(scales) if training_cfg["use_class_weight"] else None,
+                                        )
+        self.postprocess = PostProcess(
+                                        confidence_threshold=training_cfg["confidence_threshold"],
+                                        iou_threshold=training_cfg["iou_threshold"],
+                                    )
 
-                if training_cfg["save_eval_images"]:
-                    pred_classes_with_names = labels_to_classnames(
-                        pred_classes, labelmap
-                    )
-                    pred_boxes = model_output_to_image(pred_boxes.cpu(), metadata)
-                    image_with_boxes = BoxUtil.draw_box_on_image(
-                        metadata["impath"].pop(),
-                        pred_boxes,
-                        pred_classes_with_names,
-                    )
+    
+        self.training_cfg = training_cfg
+        self.metric=MeanAveragePrecision(iou_type="bbox", class_metrics=True)
+        self.epoch_train_losses = GeneralLossAccumulator()
+        self.train_loss_accumulator, self.val_loss_accumulator = [GeneralLossAccumulator()] * 2
+    def forward(self, x):
+        return self.model(x)
 
-                    write_png(image_with_boxes, f"debug/{epoch}/{i}.jpg")
+    def training_step(self, batch, batch_idx):
+        image,labels,boxes,metadata=batch
+        all_pred_boxes, _, pred_sims, _ = self(image)
+        losses = self.criterion(pred_sims, labels, all_pred_boxes, boxes)
+        loss = (
+            losses["loss_ce"]
+            + losses["loss_bg"]
+            + losses["loss_bbox"]
+            + losses["loss_giou"]
+        )
+        self.log("train_loss", loss)
+        self.log("train_loss_ce", losses["loss_ce"])
+        self.log("train_loss_bg", losses["loss_bg"])
+        self.log("train_loss_bbox", losses["loss_bbox"])
+        self.log("train_loss_giou", losses["loss_giou"])
 
-        epoch_val_losses = val_loss_accumulator.get_values()
+        return loss
 
-        print("Computing metrics...")
-        val_metrics = metric.compute()
+    def validation_step(self, batch, batch_idx):
+        image,labels,boxes,metadata=batch
+        # Get predictions and save output
+        pred_boxes, _, pred_class_sims, _ = self(image)
+        losses = self.criterion(pred_class_sims, labels, pred_boxes, boxes)
+        self.val_loss_accumulator.update(losses)
+
+        pred_boxes, pred_classes, scores = self.postprocess(
+            pred_boxes, pred_class_sims
+        )
+
+        # Use only the top 200 boxes to stay consistent with benchmarking
+        top = torch.topk(scores, min(200, scores.size(-1)))
+        scores = top.values
+        inds = top.indices.squeeze(0)
+
+        update_metrics(
+            self.metric,
+            metadata,
+            pred_boxes[:, inds],
+            pred_classes[:, inds],
+            scores,
+            boxes,
+            labels,
+        )
+
+        if self.training_cfg["save_eval_images"]:
+            pred_classes_with_names = labels_to_classnames(
+                pred_classes, self.labelmap
+            )
+            pred_boxes = model_output_to_image(pred_boxes.cpu(), metadata)
+            image_with_boxes = BoxUtil.draw_box_on_image(
+                metadata["impath"].pop(),
+                pred_boxes,
+                pred_classes_with_names,
+            )
+
+            write_png(image_with_boxes, f"debug/{self.current_epoch}/{batch_idx}.jpg")
+        return losses
+    
+    def validation_epoch_end(self, outputs):
+        classMAPs = {v: [] for v in list(self.labelmap.values())}
+        #epoch_val_losses = val_loss_accumulator.get_values()
+
+        val_metrics = self.metric.compute()
         for i, p in enumerate(val_metrics["map_per_class"].tolist()):
-            label = labelmap[str(i)]
+            label = self.labelmap[str(i)]
             classMAPs[label].append(p)
 
         with open("class_maps.json", "w") as f:
             json.dump(classMAPs, f)
 
-        metric.reset()
-        progress_summary.update(
-            epoch, epoch_train_losses, epoch_val_losses, val_metrics
-        )
+        self.metric.reset()
 
-        progress_summary.print()
+
+    def configure_optimizers(self):        
+        optimizer = torch.optim.AdamW(
+            [p for n,p in self.model.named_parameters() if any( [
+            "box" in n,
+            "post_layernorm" in n,
+            "class_predictor" in n,
+            "queries" in n,
+        ]) ],
+            lr=float(self.training_cfg["learning_rate"]),
+            weight_decay=self.training_cfg["weight_decay"],
+            amsgrad=True,
+        )
+        
+        scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.9)
+        return optimizer, scheduler
+
+if __name__ == "__main__":
+    
+
+    training_cfg = get_training_config()
+    train_dataloader, test_dataloader, scales, labelmap = get_dataloaders()
+
+    os.environ["TOKENIZERS_PARALLELISM"] = "false"
+
+    module = OwlVITModule(training_cfg,scales, labelmap)
+
+
+    trainer = pl.Trainer(
+                         precision=16,
+                         max_epochs=2,#args['epochs'], 
+                         num_sanity_val_steps=0,
+                         gradient_clip_val=0.25,
+                         accumulate_grad_batches=4,
+                         #callbacks=[ModelCheckpoint(dirpath=args['output_dir'],save_top_k=1,monitor='val_loss',mode='min')],
+                         accelerator='auto',
+                         fast_dev_run=True,  
+                         devices="auto",
+                            )
+    trainer.fit(model,train_dataloader)
+    trainer.test(model,test_dataloader)
+
